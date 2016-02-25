e1eb2e3b3ef45e902a46581409f8ad779044904b
Mario Cecchi <macecchi@gmail.com>
2016-02-24 21:47:02 -0300

    Fixing environment variables names

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
  */
 module.exports = {
     database: {
-        dbName: process.env.NODEJS_DB_NAME  || 'nodejs',
-        host: process.env.NODEJS_DB_HOST    || 'localhost',
-        port: process.env.NODEJS_DB_PORT    || 27017,
-        user: process.env.NODEJS_DB_USER    || '',
-        pass: process.env.NODEJS_DB_PASS    || ''
+        dbName: process.env.RIOBUS_DB_NAME  || 'nodejs',
+        host: process.env.RIOBUS_DB_HOST    || 'localhost',
+        port: process.env.RIOBUS_DB_PORT    || 27017,
+        user: process.env.RIOBUS_DB_USER    || '',
+        pass: process.env.RIOBUS_DB_PASS    || ''
     },
     schema: {
         itineraryCollection: 'itinerary'
